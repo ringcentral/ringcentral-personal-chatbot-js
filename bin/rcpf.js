ad801765766567755c87b946017ea7dc8482ef2a
ZHAO Xudong <zxdong@gmail.com>
2019-05-19 18:01:10 +0800

    Fix server path

--- bin/rcpf.js
+++ bin/rcpf.js
@@ -2,12 +2,11 @@
 require('dotenv').config()
 const { resolve } = require('path')
 const program = require('commander')
-const { existsSync } = require('fs')
 
 const prodRunBotPath = resolve(__dirname, '../dist/server/run-bot.js')
 const prodRunBotDevPath = resolve(__dirname, '../src/server/run-bot.js')
 
-const runBot = existsSync(prodRunBotPath)
+const runBot = process.env.NODE_ENV === 'production'
   ? require(prodRunBotPath).default
   : require(prodRunBotDevPath).default
 

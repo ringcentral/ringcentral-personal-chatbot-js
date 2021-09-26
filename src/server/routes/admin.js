325ff811ef84a27c8e0e5bc11cf173a29a16583b
ZHAO Xudong <zxdong@gmail.com>
2021-09-26 19:55:31 +0800

    Update admin function

--- src/server/routes/admin.js
+++ src/server/routes/admin.js
@@ -10,13 +10,7 @@ export const initDb = async (req, res) => {
 // create database tables if not exists
 export const viewDb = async (req, res) => {
   const users = await User.findAll()
-  let result = ''
-  for (const user of users) {
-    result += `<pre>\n${JSON.stringify(user, null, 2)}\n</pre>\n`
-    const subscriptions = await user.getSubscriptions()
-    result += `<pre>\n${JSON.stringify(subscriptions, null, 2)}\n</pre>\n`
-  }
-  res.send(result)
+  res.send(users)
 }
 
 export const renewToken = renew
